5850a59ec9a2cf60dfb149d9b94a6070c33adb5b
lubeilin <1791778603@qq.com>
2024-02-29 22:28:20 +0800

    [mio] 去除多余代码

--- vnt/src/lib.rs
+++ vnt/src/lib.rs
@@ -1,14 +1,10 @@
-use crate::error::Error;
 pub const VNT_VERSION: &'static str = env!("CARGO_PKG_VERSION");
-pub type Result<T> = std::result::Result<T, Error>;
 
 pub mod channel;
 pub mod cipher;
 pub mod core;
-pub mod error;
 pub mod external_route;
 pub mod handle;
-pub mod igmp_server;
 #[cfg(feature = "ip_proxy")]
 pub mod ip_proxy;
 pub mod nat;
@@ -16,3 +12,5 @@ pub mod proto;
 pub mod protocol;
 pub mod tun_tap_device;
 pub mod util;
+
+pub use handle::callback::{DeviceInfo, ErrorInfo, HandshakeInfo, RegisterInfo, VntCallback};
