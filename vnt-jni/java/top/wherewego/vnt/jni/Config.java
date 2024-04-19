d916fd757366bed4c7a1e74b9f75c79e2ba1d7e3
lubeilin <1791778603@qq.com>
2024-04-19 16:17:39 +0800

    去除多余字段

--- vnt-jni/java/top/wherewego/vnt/jni/Config.java
+++ vnt-jni/java/top/wherewego/vnt/jni/Config.java
@@ -174,13 +174,6 @@ public class Config {
         this.relay = relay;
     }
 
-    public String getDeviceId() {
-        return deviceId;
-    }
-
-    public void setDeviceId(String deviceId) {
-        this.deviceId = deviceId;
-    }
 
     public String getServer() {
         return server;
