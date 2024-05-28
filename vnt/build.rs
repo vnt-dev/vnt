b7532e89c2a254764108051721a2a91d2e1d60e5
lbl8603 <49143209+lbl8603@users.noreply.github.com>
2024-05-28 22:48:09 +0800

    简化条件编译

--- vnt/build.rs
+++ vnt/build.rs
@@ -1,4 +1,17 @@
+use cfg_aliases::cfg_aliases;
+
 fn main() {
+    cfg_aliases! {
+        cipher: {
+            any(feature = "aes_gcm",
+            feature = "chacha20_poly1305",
+            feature = "server_encrypt",
+            feature = "aes_cbc",
+            feature = "aes_ecb",
+            feature = "sm4_cbc"
+        )},
+    }
+
     std::fs::create_dir_all("src/proto").unwrap();
     protobuf_codegen::Codegen::new()
         .pure()
