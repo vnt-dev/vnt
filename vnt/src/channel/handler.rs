d7266926d8787fb85c08b6c210ef384f192adb55
lbl8603 <49143209+lbl8603@users.noreply.github.com>
2024-07-01 23:03:52 +0800

    增加Sync

--- vnt/src/channel/handler.rs
+++ vnt/src/channel/handler.rs
@@ -1,7 +1,7 @@
 use crate::channel::context::ChannelContext;
 use crate::channel::RouteKey;
 
-pub trait RecvChannelHandler: Clone + Send + 'static {
+pub trait RecvChannelHandler: Clone + Send + Sync + 'static {
     fn handle(
         &self,
         buf: &mut [u8],
