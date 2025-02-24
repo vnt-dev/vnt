ef48007922006ee7eb0755d097d96fb0f1dd2ded
lbl <1791778603@qq.com>
2025-02-24 10:57:11 +0800

    fix type error

--- vnt/src/handle/recv_data/server.rs
+++ vnt/src/handle/recv_data/server.rs
@@ -390,24 +390,15 @@ impl<Call: VntCallback, Device: DeviceWrite> ServerPacketHandler<Call, Device> {
                                         "device_fd == 0".into(),
                                     ));
                                 } else {
-                                    match tun_rs::SyncDevice::from_fd(device_fd as _) {
-                                        Ok(device) => {
-                                            if let Err(e) = self.tun_device_helper.start(
-                                                Arc::new(device),
-                                                self.config_info.allow_wire_guard,
-                                            ) {
-                                                self.callback.error(ErrorInfo::new_msg(
-                                                    ErrorType::FailedToCrateDevice,
-                                                    format!("{:?}", e),
-                                                ));
-                                            }
-                                        }
-                                        Err(e) => {
-                                            self.callback.error(ErrorInfo::new_msg(
-                                                ErrorType::FailedToCrateDevice,
-                                                format!("{:?}", e),
-                                            ));
-                                        }
+                                    let device = tun_rs::SyncDevice::from_fd(device_fd as _);
+                                    if let Err(e) = self.tun_device_helper.start(
+                                        Arc::new(device),
+                                        self.config_info.allow_wire_guard,
+                                    ) {
+                                        self.callback.error(ErrorInfo::new_msg(
+                                            ErrorType::FailedToCrateDevice,
+                                            format!("{:?}", e),
+                                        ));
                                     }
                                 }
                             }
