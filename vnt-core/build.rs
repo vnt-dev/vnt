6029fd113d42fee7aa8081b39c87f7384c71420c
lbl <1791778603@qq.com>
2026-08-22 01:27:28 +0800

    build(vnt-core): 使用 vendored protoc,不再依赖系统安装

--- vnt-core/build.rs
+++ vnt-core/build.rs
@@ -1,6 +1,13 @@
 fn main() {
+    let protoc_path = protoc_bin_vendored::protoc_bin_path()
+        .expect("failed to find vendored protoc");
+
     let mut config = prost_build::Config::new();
+
+    config.protoc_executable(protoc_path);
+
     config.protoc_arg("--experimental_allow_proto3_optional");
+
     config
         .compile_protos(
             &[
