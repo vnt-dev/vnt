e216416e2144b03c643c11cc37706dbf17c335a8
lbl <1791778603@qq.com>
2026-08-22 23:16:41 +0800

    fix(build): fall back to system protoc

--- vnt-ipc/build.rs
+++ vnt-ipc/build.rs
@@ -1,10 +1,17 @@
 fn main() {
-    let protoc_path =
-        protoc_bin_vendored::protoc_bin_path().expect("failed to find vendored protoc");
-
     let mut config = prost_build::Config::new();
 
-    config.protoc_executable(protoc_path);
+    match protoc_bin_vendored::protoc_bin_path() {
+        Ok(protoc_path) => {
+            config.protoc_executable(protoc_path);
+        }
+        Err(error) => {
+            println!(
+                "cargo:warning=vendored protoc unavailable ({error:?}); falling back to protoc from PATH"
+            );
+        }
+    }
+
     config.protoc_arg("--experimental_allow_proto3_optional");
     config
         .compile_protos(&["proto/local_ipc.proto"], &["proto"])
